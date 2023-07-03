bd5693e2057d4a288c1a2cfd27fecd1f0e12cbf2
RajshriSkillup <rajashreep@skillup.online>
2023-07-03 06:59:44 -0400

    work done

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,11 +1,11 @@
 from deep_translator import MyMemoryTranslator
 
 def englishToFrench(englishText):
-    translator = MyMemoryTranslator(source='en', target='fr')
+    translator = MyMemoryTranslator(source='en-US', target='fr-FR')
     frenchText = translator.translate(englishText)
     return frenchText
 
 def frenchToEnglish(frenchText):
-    translator = MyMemoryTranslator(source='fr', target='en')
+    translator = MyMemoryTranslator(source='fr-FR', target='en-GB')
     englishText = translator.translate(frenchText)
     return englishText
